f784cf95e0f2d701f87208344ff23a543569590f
Data-drone <bpl.law@gmail.com>
2016-12-01 16:41:10 +0000

    make model 2 feat gen and R eda on factor levels

---
+++ EDA.R
@@ -0,0 +1,45 @@
+
+require(dplyr)
+require(readr)
+require(ggplot2)
+require(caret)
+
+data_path = '../Dropbox/Kaggle_AllState/'
+
+train_frame <- read_csv(paste0(data_path, 'train.csv'))
+
+
+train_frame %>%
+  select()
+
+nzr <- nearZeroVar(train_frame, saveMetrics= TRUE)
+
+filtered_train <- train_frame[, -nzr$nzv]
+
+T2 <- filtered_train %>%
+  select(contains("cat"), loss) %>%
+  mutate_each(funs(factor), starts_with("cat*"))
+
+merged_frame <- train_frame %>% 
+  select(id, loss) %>%
+  mutate(centers = merge)
+
+
+require(ggiraph)
+
+merged_frame %>%
+  ggplot(aes(x=loss)) +
+  geom_histogram() +
+  facet_grid(. ~ centers, scales = "free") +
+  geom_bar_interactive()
+
+
+
+TT <- merged_frame %>%
+  filter(centers == 1)
+summary(TT$loss)
+
+T3 <- train_frame %>%
+  select(id, cat100, loss) %>%
+  ggplot(aes(x=cat100, y=log(loss) ) ) +
+  geom_boxplot()
